d19278ae3639db4af321150aa5629f1a61f5661f
Claudio Sandro Quispesivana Torres <u202215099@upc.edu.pe>
2025-06-21 15:44:10 -0500

    feat: profile added

--- src/app/profile/components/doctor-profile/doctor-profile.component.ts
+++ src/app/profile/components/doctor-profile/doctor-profile.component.ts
@@ -11,4 +11,16 @@ export class DoctorProfileComponent implements OnInit {
   @Input() doctor!: DoctorProfile;
 
   ngOnInit(): void {}
+
+  getAge(birthday: string): number {
+    if (!birthday) return 0;
+    const birthDate = new Date(birthday);
+    const today = new Date();
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+    return age;
+  }
 }
